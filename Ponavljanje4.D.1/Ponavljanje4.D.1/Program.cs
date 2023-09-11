ebce87994205bff210fa4c4652cd9b2db3b801c6
Raul-Holik <113007938+Raul-Holik@users.noreply.github.com>
2023-09-11 11:27:05 +0200

    Add files via upload

---
+++ Ponavljanje4.D.1/Ponavljanje4.D.1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ponavljanje4.D._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 100;
+            int b = 3;
+            float x, y;
+
+            x = a / b;
+            y = a % b;
+
+            Console.WriteLine("A dijelejno s b je "+x+"i ostatak je "+y);
+            Console.ReadKey();
+        }
+    }
+}
